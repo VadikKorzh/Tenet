765954e65414e34b477b5b89411c2be4991230f7
VadikKorzh <vadikkorzh@gmail.com>
2017-07-31 14:06:16 +0300

    Add project files.

---
+++ Buzzword/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Buzzword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Coachella.SingleMinded _dataProvider;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            String connString = ConfigurationManager.ConnectionStrings["VadiKars"].ConnectionString;
+            String provider = ConfigurationManager.ConnectionStrings["VadiKars"].ProviderName;
+
+            _dataProvider = new Coachella.SingleMinded();
+            _dataProvider.OpenConnection(provider, connString);
+        }
+
+        private void populateSimpleButton_Click(object sender, RoutedEventArgs e)
+        {
+            //GetDataViaReader();
+            this.gridControl.ItemsSource = _dataProvider.GetAllRecords(TableNameTextEdit.Text);
+        }
+
+        private void GetDataViaReader()
+        {
+            
+        }
+
+
+        private void GetDataViaAdapter()
+        {
+            
+        }
+
+        private void connInfoSimpleButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ Coachella/Serene.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coachella
+{
+    public class SingleMinded
+    {
+        private DbProviderFactory _providerFactory;
+        private DbConnection _connection;
+
+        public void OpenConnection(String provider, String connString)
+        {
+            _providerFactory = DbProviderFactories.GetFactory(provider);
+
+            _connection = _providerFactory.CreateConnection();
+            _connection.ConnectionString = connString;
+            _connection.Open();
+        }
+
+        public DataTable GetAllRecords(String tableName)
+        {
+            DataTable resultingTable = new DataTable(tableName);
+            using (DbCommand cmd = _providerFactory.CreateCommand())
+            {
+                cmd.CommandText = $"select * from {tableName}";
+                cmd.Connection = _connection;
+                using (DbDataAdapter da = _providerFactory.CreateDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.Fill(resultingTable);
+                }
+            }
+            return resultingTable;
+        }
+
+        public void CloseConnection()
+        {
+            _connection?.Close();
+        }
+    }
+}
